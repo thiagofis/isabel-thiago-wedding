74329f70e3f2640402f36cbbb263f187327c381e
Thiago Marcolino da Silva <thiago.marcolino@thetrainline.com>
2019-05-17 18:28:21 +0100

    first commit

---
+++ src/main/java/thiago/isabel/wedding/controllers/LadingPageController.java
@@ -0,0 +1,13 @@
+package thiago.isabel.wedding.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LadingPageController {
+
+    @GetMapping("/index")
+    public void index() {
+
+    }
+}
---
+++ src/test/java/thiago/isabel/wedding/WeddingApplicationTests.java
@@ -0,0 +1,16 @@
+package thiago.isabel.wedding;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeddingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
