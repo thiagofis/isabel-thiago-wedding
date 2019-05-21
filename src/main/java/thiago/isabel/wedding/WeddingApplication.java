c2972469b16977aaa03964549c8099ca0329bd1a
Thiago Marcolino da Silva <thiago.marcolino@thetrainline.com>
2019-05-21 20:39:26 +0100

    updates

--- src/main/java/thiago/isabel/wedding/WeddingApplication.java
+++ src/main/java/thiago/isabel/wedding/WeddingApplication.java
@@ -2,6 +2,7 @@ package thiago.isabel.wedding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -11,4 +12,10 @@ public class WeddingApplication extends SpringBootServletInitializer {
         SpringApplication.run(WeddingApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<WeddingApplication> applicationClass = WeddingApplication.class;
 }
